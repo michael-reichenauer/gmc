66ac28f567443c816ce5a9f9c47a21a7eb2d07dc
Michael Reichenauer <michael.reichenauer@gmail.com>
2019-11-17 08:40:56 +0100

    adjust build

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"flag"
+	"github.com/mattn/go-isatty"
 	"github.com/michael-reichenauer/gmc/repoview"
 	"github.com/michael-reichenauer/gmc/utils"
 	"github.com/michael-reichenauer/gmc/utils/ui"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 var (
 	repoPath = flag.String("d", "", "Specify directory")
-	external = flag.Bool("external", false, "Specify directory")
 )
 
 var version string
@@ -19,23 +20,25 @@ var version string
 func main() {
 	flag.Parse()
 	if *repoPath == "" {
+		// No specified repo path, use current dir
 		*repoPath = utils.CurrentDir()
 	}
-	//*repoPath = `C:\Work Files\GitMind`
-	if *external {
-		var arg []string
-		arg = append(arg, "/C")
-		arg = append(arg, "start")
-		for i := 0; i < len(os.Args); i++ {
-			if os.Args[i] != "-external" {
-				arg = append(arg, os.Args[i])
-			}
+
+	if !isatty.IsTerminal(os.Stdout.Fd()) && !isatty.IsCygwinTerminal(os.Stdout.Fd()) && runtime.GOOS == "windows" {
+		// Seems to be not running in a terminal like e.g. in goland,
+		// termbox requires a terminal, so lets restart as an external command on windows
+		var args []string
+		args = append(args, "/C")
+		args = append(args, "start")
+		for _, arg := range os.Args {
+			args = append(args, arg)
 		}
-		cmd := exec.Command("cmd", arg...)
+		cmd := exec.Command("cmd", args...)
 		_ = cmd.Start()
 		_ = cmd.Wait()
 		return
 	}
+
 	uiHandler := ui.NewUI()
 	uiHandler.Run(func() {
 		repoView := repoview.New(uiHandler, *repoPath)
