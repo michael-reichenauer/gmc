a98aabcb131895b3f5dbdeae849ce6a134c20740
Michael Reichenauer <michael.reichenauer@gmail.com>
2019-12-14 11:45:22 +0100

    use parsed branch name to reduce multi branches

--- repoview/model/gitmodel/model.go
+++ repoview/model/gitmodel/model.go
@@ -2,6 +2,8 @@ package gitmodel
 
 import (
 	"github.com/michael-reichenauer/gmc/utils/git"
+	"github.com/michael-reichenauer/gmc/utils/log"
+	"strings"
 	"sync"
 )
 
@@ -105,8 +107,12 @@ func (h *Handler) determineCommitBranches(repo *Repo) {
 }
 
 func (h *Handler) determineBranch(repo *Repo, c *Commit) {
+	if strings.HasPrefix(c.Id, "955") {
+		log.Debugf("")
+	}
 	if c.Branch != nil {
-		// Commit already knows its branch, e.g. deleted merged branch
+		// Commit already knows its branch
+		panic("Commit already knows its branch") // ##############?????????
 		return
 	}
 
@@ -116,64 +122,129 @@ func (h *Handler) determineBranch(repo *Repo, c *Commit) {
 		return
 	}
 
-	if len(c.Branches) == 2 {
-		if c.Branches[0].IsRemote && c.Branches[0].Name == c.Branches[1].RemoteName {
-			// remote and local branch, prefer remote
-			c.Branch = c.Branches[0]
-			return
-		}
-		if !c.Branches[0].IsRemote && c.Branches[0].RemoteName == c.Branches[1].Name {
-			// local and remote branch, prefer remote
-			c.Branch = c.Branches[1]
-			return
-		}
+	if branch := h.isLocalRemoteBranch(c); branch != nil {
+		// local and remote branch, prefer remote
+		c.Branch = branch
+		return
 	}
 
+	if branch := h.isMergedDeletedBranch(repo, c); branch != nil {
+		// Commit has no branch, must be a deleted branch tip merged into some other branch
+		c.Branch = branch
+		c.Branches = append(c.Branches, c.Branch)
+		return
+	}
+
+	if len(c.Branches) == 0 && len(c.Children) == 1 {
+		// commit has no known branches (middle commit in deleted branch), but has one child, use that branch
+		c.Branch = c.Children[0].Branch
+		c.Branches = append(c.Branches, c.Branch)
+		return
+	}
+
+	if len(c.Children) == 1 && c.Children[0].IsLikely {
+		c.Branch = c.Children[0].Branch
+		c.IsLikely = true
+		return
+	}
+
+	if branch := h.hasPriorityBranch(c); branch != nil {
+		// Commit, has many possible branches, check if one is in the priority list, e.g. master, develop, ...
+		c.Branch = branch
+		return
+	}
+
+	if name := h.branchNames.branchName(c.Id); name != "" {
+		// The commit branch name could be parsed form the subject (or a child subject)
+		// Lets use that as a branch and also let children use that branch if they only are multi branch
+		var current *Commit
+		for current = c; len(current.Children) == 1 && current.Children[0].Branch.IsMultiBranch; current = current.Children[0] {
+		}
+		branch := h.tryGetBranchFromName(c, name)
+		if branch == nil {
+			branch = repo.AddNamedBranch(current, name)
+		}
+		for current = current.Parent; current != c; current = current.Parent {
+			current.Children[0].Branch = branch
+		}
+		c.Branch = branch
+		c.Branch.BottomID = c.Id
+		c.IsLikely = true
+		return
+	}
+
+	if branch := h.isChildMultiBranch(c); branch != nil {
+		// one of the commit children is a multi branch, reuse
+		c.Branch = branch
+		c.Branches = append(c.Branches, c.Branch)
+		return
+	}
+
+	// Commit, has several possible branches, create a new multi branch
+	c.Branch = repo.AddMultiBranch(c)
+	c.Branches = append(c.Branches, c.Branch)
+}
+
+func (h *Handler) hasPriorityBranch(c *Commit) *Branch {
+	if len(c.Branches) < 1 {
+		return nil
+	}
+	for _, bp := range DefaultBranchPrio {
+		for _, cb := range c.Branches {
+			if bp == cb.Name {
+				return cb
+			}
+		}
+	}
+	return nil
+}
+func (h *Handler) isChildMultiBranch(c *Commit) *Branch {
+	for _, cc := range c.Children {
+		if cc.Branch.IsMultiBranch {
+			// one of the commit children is a multi branch
+			return cc.Branch
+		}
+	}
+	return nil
+}
+
+func (h *Handler) tryGetBranchFromName(c *Commit, name string) *Branch {
+	for _, b := range c.Branches {
+		if name == b.DisplayName {
+			return b
+		}
+	}
+	return nil
+}
+
+func (h *Handler) isMergedDeletedBranch(repo *Repo, c *Commit) *Branch {
 	if len(c.Branches) == 0 && len(c.Children) == 0 {
 		// Commit has no branch, must be a deleted branch tip merged into some branch or unusual branch
 		// Trying to ues parsed branch name from one of the merge children subjects e.g. Merge branch 'a' into develop
 		name := h.branchNames.branchName(c.Id)
 		if name != "" {
 			// Managed to parse a branch name
-			c.Branch = repo.AddNamedBranch(c, name)
-			c.Branches = append(c.Branches, c.Branch)
-			return
+			return repo.AddNamedBranch(c, name)
 		}
 
 		// could not parse a name from any of the merge children, use id named branch
-		c.Branch = repo.AddIdNamedBranch(c)
-		c.Branches = append(c.Branches, c.Branch)
-		return
+		return repo.AddIdNamedBranch(c)
 	}
-	if len(c.Branches) == 0 && len(c.Children) == 1 {
-		// commit has no known branches, but has one child, use that branch
-		c.Branch = c.Children[0].Branch
-		c.Branches = append(c.Branches, c.Branch)
-		return
-	}
+	return nil
+}
 
-	// Commit, has many possible branches, check if one is in the priority list, e.g. master, develop, ...
-	for _, bp := range DefaultBranchPrio {
-		for _, cb := range c.Branches {
-			if bp == cb.Name {
-				c.Branch = cb
-				return
-			}
+func (h *Handler) isLocalRemoteBranch(c *Commit) *Branch {
+	if len(c.Branches) == 2 {
+		if c.Branches[0].IsRemote && c.Branches[0].Name == c.Branches[1].RemoteName {
+			// remote and local branch, prefer remote
+			return c.Branches[0]
+		}
+		if !c.Branches[0].IsRemote && c.Branches[0].RemoteName == c.Branches[1].Name {
+			// local and remote branch, prefer remote
+			return c.Branches[1]
 		}
 	}
-
-	for _, cc := range c.Children {
-		if cc.Branch.IsMultiBranch {
-			// one of the commit children is a multi branch, reuse
-			c.Branch = cc.Branch
-			c.Branches = append(c.Branches, c.Branch)
-			return
-		}
-	}
-
-	// Commit, has many possible branches and many children, create a new multi branch
-	c.Branch = repo.AddMultiBranch(c)
-	c.Branches = append(c.Branches, c.Branch)
+	return nil
 }
 
 func (h *Handler) determineBranchHierarchy(repo *Repo) {
