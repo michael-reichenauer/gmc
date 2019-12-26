e36e610bd1a5157bda4c3dc28629ef368e54e163
Michael Reichenauer <michael.reichenauer@gmail.com>
2019-12-26 06:46:40 +0100

    improve branch name parsing

--- repoview/viewmodel/gitmodel/branchnames_test.go
+++ repoview/viewmodel/gitmodel/branchnames_test.go
@@ -11,10 +11,39 @@ func TestParseBranchNames(t *testing.T) {
 	fi := h.parseCommit(c("1", "Merge branch 'develop' into master", "2", "3"))
 	assert.Equal(t, "develop", fi.from)
 	assert.Equal(t, "master", fi.into)
+}
 
-	fi = h.parseCommit(c("2", "Merge branch 'develop'", "3", "4"))
+func TestParseSubject(t *testing.T) {
+	h := newBranchNames()
+
+	fi := h.parseMergeBranchNames("Merge branch 'develop' into master")
+	assert.Equal(t, "develop", fi.from)
+	assert.Equal(t, "master", fi.into)
+
+	fi = h.parseMergeBranchNames("Merged branch 'develop' into master")
+	assert.Equal(t, "develop", fi.from)
+	assert.Equal(t, "master", fi.into)
+
+	fi = h.parseMergeBranchNames("Merged commit 'develop' into master")
+	assert.Equal(t, "develop", fi.from)
+	assert.Equal(t, "master", fi.into)
+
+	fi = h.parseMergeBranchNames("Merged 'develop' into master")
+	assert.Equal(t, "develop", fi.from)
+	assert.Equal(t, "master", fi.into)
+
+	fi = h.parseMergeBranchNames("Merge branch 'develop'")
 	assert.Equal(t, "develop", fi.from)
 	assert.Equal(t, "", fi.into)
+
+	fi = h.parseMergeBranchNames("Merge branch develop")
+	assert.Equal(t, "develop", fi.from)
+	assert.Equal(t, "", fi.into)
+
+	// Pull merge
+	fi = h.parseMergeBranchNames("Merge branch 'master' of https://sa-git/git/sa/Products/AcmAcs")
+	assert.Equal(t, "master", fi.from)
+	assert.Equal(t, "master", fi.into)
 }
 
 func c(id, subject string, parents ...string) *Commit {
