00b311038cd8c8c376023da3780201e408365503
Michael Reichenauer <michael-reichenauer@users.noreply.github.com>
2022-09-24 04:20:37 +0000

    Go version 1.19

--- server/viewrepo/viewrepo_test.go
+++ server/viewrepo/viewrepo_test.go
@@ -1,9 +1,21 @@
 package viewrepo
 
 import (
-	"github.com/michael-reichenauer/gmc/utils/tests"
 	"testing"
+
+	"github.com/michael-reichenauer/gmc/utils/tests"
+	"github.com/samber/lo"
 )
+
+func Test2(t *testing.T) {
+	t.Logf("Test")
+
+	even := lo.Filter[int]([]int{1, 2, 3, 4}, func(x int, _ int) bool {
+		return x%2 == 0
+	})
+	t.Logf("array %v", even)
+	// []int{2, 4}
+}
 
 func Test(t *testing.T) {
 	tests.ManualTest(t)
